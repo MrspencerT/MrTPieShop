659b6d0a5d4d1ae0d3d67d8028bf75d8f5878316
MrspencerT <spencertavion@yahoo.com>
2022-12-11 23:31:24 -0500

    clear code

--- Program.cs
+++ Program.cs
@@ -1,5 +1,8 @@
-var builder = WebApplication.CreateBuilder(args);
+using BethanyPieShop.Models;
 
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
+builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
