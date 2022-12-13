4d93e6aba941ddeaf2ced1bcb9cbe480a83c1e37
MrspencerT <spencertavion@yahoo.com>
2022-12-13 15:13:56 -0500

    added EfCore to Application

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 using BethanyPieShop.Models;
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
 //custom scope to di container
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
 builder.Services.AddScoped<IPieRepository, MockPieRepository>();
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
 
 var app = builder.Build();
 //looks for static files wwwroot
