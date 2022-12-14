8075418af08277582f7495e252b1911328c883f8
MrspencerT <spencertavion@yahoo.com>
2022-12-14 11:55:55 -0500

    added real repo

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 using BethanyPieShop.Models;
 using BethanysPieShop.Models;
 using Microsoft.EntityFrameworkCore;
+
 //custom scope to di container
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddScoped<ICategoryRepository, MockCategoryRepository>();
-builder.Services.AddScoped<IPieRepository, MockPieRepository>();
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IPieRepository, PieRepository>();
 builder.Services.AddControllersWithViews();
 
+//resgistering database 
 builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
 {
+   //concat from connection string and bethanys pie shop
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
