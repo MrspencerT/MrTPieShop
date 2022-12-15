3c6c8ceb9a107230e77e300a8b37760af0c9f78e
MrspencerT <spencertavion@yahoo.com>
2022-12-15 13:05:54 -0500

    finshed creating shopping cart

---
+++ ViewModels/ShoppingCartViewModel1.cs
@@ -0,0 +1,16 @@
+﻿using BethanyPieShop.Models;
+
+namespace BethanysPieShop.Controllers
+{
+    internal class ShoppingCartViewModel
+    {
+        private IShoppingCart shoppingCart;
+        private decimal v;
+
+        public ShoppingCartViewModel(IShoppingCart shoppingCart, decimal v)
+        {
+            this.shoppingCart = shoppingCart;
+            this.v = v;
+        }
+    }
+}
