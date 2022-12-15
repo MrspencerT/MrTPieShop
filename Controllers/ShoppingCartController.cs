056e00f23a910f25732211962e827c091b1dad39
MrspencerT <spencertavion@yahoo.com>
2022-12-15 12:19:59 -0500

    added Shopping cart Logic and  Controller Helpers

---
+++ Controllers/ShoppingCartController.cs
@@ -0,0 +1,17 @@
+﻿using BethanyPieShop.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethanyPieShop.Controllers
+{
+    public class ShoppingCartController: Controller
+    {
+        private readonly IPieRepository _pieRepository;
+        private readonly IShoppingCart _shoppingCart;
+
+        public ShoppingCartController(IShoppingCart shoppingCart, IPieRepository pieRepository)
+        {
+            _shoppingCart = shoppingCart;
+            _pieRepository = pieRepository;
+        }
+    }
+}
