64973d2719c98b40b3fdc4304303981311dca413
MrspencerT <spencertavion@yahoo.com>
2022-12-22 18:29:24 -0500

    Finished acessing posted data using model binding

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -13,8 +13,34 @@ namespace BethanyPieShop.Controllers
             _shoppingCart = shoppingCart;   
         }
 
-        public IActionResult Checkout()
+        public IActionResult Checkout()//Get request default
         {
+            return View();
+        }
+
+        [HttpPost] //called when a post is recieved 
+        public IActionResult Checkout(Order order)
+        {
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            if (_shoppingCart.ShoppingCartItems.Count == 0)
+            {
+                ModelState.AddModelError("", "Your cart is empty, add some pies first");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _orderRepository.CreateOrder(order);
+                _shoppingCart.ClearCart();
+                return RedirectToAction("CheckoutComplete");
+            }
+            return View(order);
+        }
+        
+        public IActionResult CheckoutComplete()
+        {   
+            ViewBag.CheckoutCompleteMessage = "Thanks for your order. You'll soon enjoy our delicious pies!";
             return View();
         }
 
