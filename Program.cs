8039a6208f1f260f2e7acdee1c5fac80e7b48a07
MrspencerT <spencertavion@yahoo.com>
2023-01-16 17:24:17 -0500

    Added Authenication Login and Register tooling

--- Program.cs
+++ Program.cs
@@ -2,8 +2,10 @@ using BethanyPieShop.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileSystemGlobbing.Internal.Patterns;
 using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Identity;
 //custom scope to di container
 var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BethanysPieShopDbContextConnection") ?? throw new InvalidOperationException("Connection string 'BethanysPieShopDbContextConnection' not found.");
 builder.Services.AddControllersWithViews()
         .AddJsonOptions(options =>
         {
@@ -26,6 +28,9 @@ builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
     options.UseSqlServer(
         builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
 });
+
+builder.Services.AddDefaultIdentity<IdentityUser>()
+    .AddEntityFrameworkStores<BethanysPieShopDbContext>();
 
 builder.Services.AddServerSideBlazor();
 // we have addwithviews dont need for api:builder.Services.AddControllers();  
